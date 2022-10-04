3a758d079c1d91c5ea1b8363c9b663620bbe3dc0
Zubair Auhammud <72204490+zub320@users.noreply.github.com>
2022-10-04 19:56:21 +0400

    Add algorithm for prime number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("To check if a number is prime or not. \r\n");
+Console.Write("Enter a Number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if(MyMath.IsPrime(number))
+{
+    Console.Write("Number is Prime.");
+}
+else
+{
+    Console.Write("Number is not Prime.");
+}
+
+Console.ReadKey();
